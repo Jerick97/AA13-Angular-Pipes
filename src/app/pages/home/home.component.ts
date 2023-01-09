5d7266f2e0e637c5a78b28d7a549bca534e46f9a
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-09 10:45:40 -0500

    Currency Añadido

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   name !: string;
+  currency !: number;
+  selectedOption !: string;
+  monedaElegida !: string;
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  capturarSelect(){
+    this.monedaElegida = this.selectedOption;
+  }
 }
